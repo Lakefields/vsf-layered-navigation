35929d2101d83dfc8a153e0a507bfb982a921ec9
Daan Kouters <daan@getnoticed.nl>
2019-04-12 00:25:16 +0200

    Add better way to setup extend module

--- index.ts
+++ index.ts
@@ -1,24 +1,13 @@
-// import { VueStorefrontModule, VueStorefrontModuleConfig, extendModule } from '@vue-storefront/core/lib/module'
-// import catalogCategoryExtendedModule from './store'
-// import { Catalog } from '@vue-storefront/core/modules/catalog'
+import catalogCategoryExtendedModule from './store/category/actions'
+import catalogProductExtendedModule from './store/product/actions'
 
-// const extendCatalogCategoryAfterRegistration = function () {
-//   console.log('catalog module is extended -> debug purposes')
-// }
-
-// const catalogCategoryExtend = {
-//   key: 'catalog',
-//   store: { 
-//     modules:[
-//       { 
-//         key: 'category',
-//         module: catalogCategoryExtendedModule
-//       } 
-//     ]
-//   },
-//   afterRegistration: extendCatalogCategoryAfterRegistration
-// }
-
-// extendModule(catalogCategoryExtend)
-
-// export const registerModules: VueStorefrontModule[] = [Catalog];
+const KEY = 'catalog'
+export const layeredNavigationModule = {
+  key: KEY,
+  store: { 
+    modules: [
+    { key: 'category', module: catalogCategoryExtendedModule },
+    { key: 'product', module: catalogProductExtendedModule },
+    ] 
+  }
+} 
